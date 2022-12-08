22cb5af0885dc455b68c89f46071e007f8caf7fe
Martin Martinov <m.martinov@kipo.bg>
2022-12-08 17:35:34 +0200

    input fileds

---
+++ 11 Handling User Input with Forms/src/Switch.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id="on" @click="switched(true)" :class="{ active: value }">On</div>
+    <div id="off" @click="switched(false)" :class="{ active: !value }">Off</div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["value"],
+  data: function () {
+    return {
+    };
+  },
+  methods: {
+    switched(isOn) {
+      this.$emit("input", isOn);
+    },
+  },
+};
+</script>
+<style scoped>
+#on,
+#off {
+  display: inline-block;
+  width: 40px;
+  height: 20px;
+  padding: 2px;
+  margin: 10px -2px;
+  text-align: center;
+  box-sizing: content-box;
+  background-color: lightgray;
+  cursor: pointer;
+}
+#on:hover,
+#on.active {
+  background-color: lightgreen;
+}
+#off:hover,
+#off.active {
+  background-color: lightcoral;
+}
+</style>
